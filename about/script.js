ec5775f3675056aa7e6e86c8e7e896cb93e649fa
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2022-12-31 08:06:38 +0700

    Update script.js

--- about/script.js
+++ about/script.js
@@ -5,7 +5,7 @@ const card = document.querySelector(".card");
 const handleButtonClick = (e) => {
   const targetSection = e.target.getAttribute("data-section");
   const section = document.querySelector(targetSection);
-  targetSection !== "#about" ? card.classList.add("is-active") : card.classList.remove("is-active");
+  targetSection !== "#home" ? card.classList.add("is-active") : card.classList.remove("is-active");
   card.setAttribute("data-state", targetSection);
   sections.forEach((s) => s.classList.remove("is-active"));
   buttons.forEach((b) => b.classList.remove("is-active"));
@@ -16,8 +16,3 @@ const handleButtonClick = (e) => {
 buttons.forEach((btn) => {
   btn.addEventListener("click", handleButtonClick);
 });
-
-function init() {
-  var e = document.getElementById("myAudio");
-    (e.volume = 0.15), e.play();
-};
