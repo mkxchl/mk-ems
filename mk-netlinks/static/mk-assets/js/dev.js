b73b8ee2e31ab2d517de44844acc0630bf510506
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2023-06-22 10:48:01 +0700

    Add files via upload

---
+++ mk-netlinks/static/mk-assets/js/dev.js
@@ -0,0 +1,27 @@
+var currentDateElement = document.getElementById("current-date");
+
+function updateDateTime() {
+  var currentDateTime = new Date();
+
+  var day = currentDateTime.getDate();
+  var month = currentDateTime.getMonth() + 1; // Bulan dimulai dari 0, jadi tambahkan 1
+  var year = currentDateTime.getFullYear();
+
+  var hours = currentDateTime.getHours();
+  var minutes = currentDateTime.getMinutes();
+  var seconds = currentDateTime.getSeconds();
+
+  var daysOfWeek = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+  var dayOfWeek = currentDateTime.getDay();
+
+  var formattedDateTime = daysOfWeek[dayOfWeek] + ", " + day + "/" + month + "/" + year + " " + hours + ":" + minutes + ":" + seconds;
+
+  currentDateElement.textContent = formattedDateTime;
+}
+
+// Panggil fungsi updateDateTime setiap detik
+setInterval(updateDateTime, 1000);
+
+// Mendapatkan informasi perangkat
+var deviceElement = document.getElementById("device");
+deviceElement.textContent = navigator.language;
