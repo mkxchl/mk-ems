b633b7eae699f686edd37017edb6c56e65d53fd8
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2023-07-04 07:11:24 +0700

    Update dev.js

--- mk-netlinks/static/mk-assets/js/dev.js
+++ mk-netlinks/static/mk-assets/js/dev.js
@@ -19,9 +19,29 @@ function updateDateTime() {
   currentDateElement.textContent = formattedDateTime;
 }
 
-// Panggil fungsi updateDateTime setiap detik
 setInterval(updateDateTime, 1000);
 
-// Mendapatkan informasi perangkat
 var deviceElement = document.getElementById("device");
 deviceElement.textContent = navigator.language;
+
+/// Dark Mode Function ///
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line");
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme);
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
