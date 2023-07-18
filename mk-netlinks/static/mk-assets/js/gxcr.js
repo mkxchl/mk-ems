c84520b9e41bb3802d5397fbaa5cded4be144beb
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2023-07-18 19:07:12 +0700

    Add files via upload

---
+++ mk-netlinks/static/mk-assets/js/gxcr.js
@@ -0,0 +1,88 @@
+function MkxxGacorA() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("http://185.63.253.200/");
+    }
+  });
+}
+function MkxxGacorB() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("https://www4.javhdporn.net/");
+    }
+  });
+}
+function MkxxGacorC() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("https://ukdevilz.com/");
+    }
+  });
+}
+function MkxxGacorD() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("https://pornhub.com/");
+    }
+  });
+}
