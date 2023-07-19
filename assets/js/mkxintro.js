1f8ed015143d443e4c64588a4ea4c4d160b9fb0d
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2023-07-19 18:35:28 +0700

    Create mkxintro.js

---
+++ assets/js/mkxintro.js
@@ -0,0 +1,88 @@
+function MkIntroA() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("https://www.mediafire.com/file/6xqnrfc1qcppl14/VID-20201204-WA0437.mp4/file");
+    }
+  });
+}
+function MkIntroB() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("https://www.mediafire.com/file/w0l4w1vgglcweev/VID-20201006-WA0441.mp4/file");
+    }
+  });
+}
+function MkIntroC() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("https://www.mediafire.com/file/x6fxjvduthq2aq7/video+(3).mkv/file");
+    }
+  });
+}
+function MkxxGacorD() {
+  let timerInterval;
+  Swal.fire({
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 500);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+      window.location.replace("https://pornhub.com/");
+    }
+  });
+}
