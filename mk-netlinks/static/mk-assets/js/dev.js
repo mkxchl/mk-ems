f8d95b2ad2e0b466c0dd007e1c54f96458f2590d
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2023-07-21 15:51:28 +0700

    Update dev.js

--- mk-netlinks/static/mk-assets/js/dev.js
+++ mk-netlinks/static/mk-assets/js/dev.js
@@ -1,47 +1,11 @@
-var currentDateElement = document.getElementById("current-date");
+document.body.addEventListener("click", (e) => {
+  const element = document.createElement("span");
+  element.className = "mk-cursor";
+  element.style.left = e.clientX - 10 + "px";
+  element.style.top = e.clientY - 10 + "px";
 
-function updateDateTime() {
-  var currentDateTime = new Date();
-
-  var day = currentDateTime.getDate();
-  var month = currentDateTime.getMonth() + 1; // Bulan dimulai dari 0, jadi tambahkan 1
-  var year = currentDateTime.getFullYear();
-
-  var hours = currentDateTime.getHours();
-  var minutes = currentDateTime.getMinutes();
-  var seconds = currentDateTime.getSeconds();
-
-  var daysOfWeek = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
-  var dayOfWeek = currentDateTime.getDay();
-
-  var formattedDateTime = daysOfWeek[dayOfWeek] + ", " + day + ":" + month + ":" + year + " " + hours + ":" + minutes + ":" + seconds;
-
-  currentDateElement.textContent = formattedDateTime;
-}
-
-setInterval(updateDateTime, 1000);
-
-var deviceElement = document.getElementById("device");
-deviceElement.textContent = navigator.language;
-
-/// Dark Mode Function ///
-
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "ri-sun-line";
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? "dark" : "light");
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line");
-
-if (selectedTheme) {
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
-  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme);
-}
-
-themeButton.addEventListener("click", () => {
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
+  document.body.append(element);
+  setTimeout(() => {
+    element.remove();
+  }, 5000);
 });
