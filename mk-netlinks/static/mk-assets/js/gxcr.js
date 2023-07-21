b60c8df2003a243fc9b11cb983e7e45bb5142fa1
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2023-07-21 15:51:22 +0700

    Update gxcr.js

--- mk-netlinks/static/mk-assets/js/gxcr.js
+++ mk-netlinks/static/mk-assets/js/gxcr.js
@@ -86,3 +86,43 @@ function MkxxGacorD() {
     }
   });
 }
+
+function MkxSpotifyProfile() {
+  bottomSheet.classList.add("show");
+  updateSheetHeight(100);
+  document.querySelector(".MkMusicSpotifyContenx").style.visibility = "hidden";
+  document.querySelector(".MkMusicSpotifyContenx").style.opacity = "0";
+  document.querySelector(".MkMusicSpotifyContenx").style.display = "none";
+}
+
+function MkxSpotifyMusic() {
+  bottomSheet.classList.add("show");
+  updateSheetHeight(40);
+  document.querySelector(".MkxchlMusic").style.visibility = "hidden";
+  document.querySelector(".MkxchlMusic").style.opacity = "0";
+  document.querySelector(".MkxchlMusic").style.display = "none";
+  document.querySelector(".MkxchlIconsSettings").style.visibility = "hidden";
+  document.querySelector(".MkxchlIconsSettings").style.opacity = "0";
+  document.querySelector(".MkxchlIconsSettings").style.display = "none";
+  document.querySelector(".MkxchlBodyMusic").style.visibility = "hidden";
+  document.querySelector(".MkxchlBodyMusic").style.opacity = "0";
+  document.querySelector(".MkxchlBodyMusic").style.display = "none";
+  document.querySelector(".MkxBodyProfile").style.visibility = "hidden";
+  document.querySelector(".MkxBodyProfile").style.opacity = "0";
+  document.querySelector(".MkxBodyProfile").style.display = "none";
+  document.querySelector(".MKxMusicBtnBack").style.visibility = "hidden";
+  document.querySelector(".MKxMusicBtnBack").style.opacity = "0";
+  document.querySelector(".MKxMusicBtnBack").style.display = "none";
+  document.querySelector(".MkMusicSpotifyContenx").style.visibility = "visible";
+  document.querySelector(".MkMusicSpotifyContenx").style.opacity = "1";
+  document.querySelector(".MkMusicSpotifyContenx").style.display = "inline";
+  document.querySelector("body").style.background = "#1DB954";
+  document.querySelector(".box").style.border = "none";
+  document.querySelector("body").style.transition = "1s infinite";
+}
+var MkxSongsA = document.getElementById("MkxSongsA");
+function MkSpotifyMusicPlay() {
+  if (MkxSongsA.paused) {
+    MkxSongsA.play();
+  }
+}
