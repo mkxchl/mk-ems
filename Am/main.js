dac19d7f0f27bbb38e34a38d255e98f70a0393c4
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2023-12-07 02:33:40 +0700

    Update main.js

--- Am/main.js
+++ Am/main.js
@@ -1,60 +1,177 @@
-const MkHeaderBody = document.querySelector(".MkHeaderBody");
-const MkSpotify = document.querySelector(".MkSpotify");
-const MkPreloader = document.querySelector(".MkPreloader");
-const MkLink = document.querySelector(".MkLink");
-const MkLinkOverlay = document.querySelector(".MkLinkOverlay");
-const MkLinkContentBodyLink = document.querySelector(".MkLinkContentBodyLink");
-const MkSupport = document.querySelector(".MkSupport");
-const MkSupportOverlay = document.querySelector(".MkSupportOverlay");
-const body = document.querySelector("body");
-const MkAudio = document.getElementById("MkAudio");
-
-MkAudio.onended = function () {
-  $("#MkAudio").prop("autoplay", true);
+const _0x38b8f8 = _0x2f1e;
+(function (_0x1eb8d2, _0x1a53ae) {
+  const _0x1cf135 = _0x2f1e,
+    _0x51f746 = _0x1eb8d2();
+  while (!![]) {
+    try {
+      const _0x238fdb =
+        -parseInt(_0x1cf135(0x177)) / 0x1 +
+        (parseInt(_0x1cf135(0x181)) / 0x2) * (parseInt(_0x1cf135(0x184)) / 0x3) +
+        -parseInt(_0x1cf135(0x19b)) / 0x4 +
+        -parseInt(_0x1cf135(0x18c)) / 0x5 +
+        (parseInt(_0x1cf135(0x192)) / 0x6) * (-parseInt(_0x1cf135(0x176)) / 0x7) +
+        -parseInt(_0x1cf135(0x190)) / 0x8 +
+        (parseInt(_0x1cf135(0x19c)) / 0x9) * (parseInt(_0x1cf135(0x178)) / 0xa);
+      if (_0x238fdb === _0x1a53ae) break;
+      else _0x51f746["push"](_0x51f746["shift"]());
+    } catch (_0x59c14b) {
+      _0x51f746["push"](_0x51f746["shift"]());
+    }
+  }
+})(_0x41e5, 0xc9ae2);
+const MkHeaderBody = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x175)),
+  MkSpotify = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x18e)),
+  MkPreloader = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x199)),
+  MkLink = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x18d)),
+  MkLinkOverlay = document[_0x38b8f8(0x1a5)](".MkLinkOverlay"),
+  MkLinkContentBodyLink = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x1a4)),
+  MkSupport = document["querySelector"](_0x38b8f8(0x1a1)),
+  MkSupportOverlay = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x17d)),
+  MkChatHeaderJoin = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x182)),
+  MkGame = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x1a0)),
+  MkGameOverlay = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x194)),
+  MkChat = document["querySelector"](_0x38b8f8(0x198)),
+  body = document[_0x38b8f8(0x1a5)](_0x38b8f8(0x17c)),
+  MkAudio = document["getElementById"](_0x38b8f8(0x180));
+MkAudio["onended"] = function () {
+  const _0x663aa9 = _0x38b8f8;
+  $(_0x663aa9(0x196))[_0x663aa9(0x19a)]("autoplay", !![]);
 };
+function OpenChat() {
+  const _0x3f93d6 = _0x38b8f8;
+  MkChat[_0x3f93d6(0x193)]["add"](_0x3f93d6(0x1a2)), MkChatHeaderJoin[_0x3f93d6(0x193)][_0x3f93d6(0x17b)](_0x3f93d6(0x19e));
+}
+function OpenGame() {
+  const _0x547028 = _0x38b8f8;
+  MkGame["classList"][_0x547028(0x179)](_0x547028(0x19d));
+}
+function CloseChat() {
+  const _0x4c3345 = _0x38b8f8;
+  MkChat[_0x4c3345(0x193)]["remove"](_0x4c3345(0x1a2));
+}
 function OpenLink() {
-  MkLink.classList.add("MkLinkShow");
-  MkHeaderBody.classList.add("Hide");
+  const _0x499726 = _0x38b8f8;
+  MkLink["classList"][_0x499726(0x179)]("MkLinkShow"), MkHeaderBody[_0x499726(0x193)][_0x499726(0x179)](_0x499726(0x17f));
 }
 function OpenSupport() {
-  MkSupport.classList.add("MkSupportShow");
-  MkHeaderBody.classList.add("Hide");
+  const _0x3fe49a = _0x38b8f8;
+  MkSupport[_0x3fe49a(0x193)][_0x3fe49a(0x179)](_0x3fe49a(0x19f)), MkHeaderBody[_0x3fe49a(0x193)][_0x3fe49a(0x179)](_0x3fe49a(0x17f));
+}
+function _0x41e5() {
+  const _0x208690 = [
+    ".MkPreloader",
+    "prop",
+    "2362572fsYboV",
+    "54dqReZa",
+    "MkGameShow",
+    "HideJoin",
+    "MkSupportShow",
+    ".MkGame",
+    ".MkSupport",
+    "MkChatShow",
+    "MkSpotifyShow",
+    ".MkLinkContentBodyLink",
+    "querySelector",
+    ".MkHeaderBody",
+    "4635078mQFGJc",
+    "445725OUYBrf",
+    "6726410GqZjbx",
+    "add",
+    "MkLinkShow",
+    "remove",
+    "body",
+    ".MkSupportOverlay",
+    "location",
+    "Hide",
+    "MkAudio",
+    "42414Rxcadh",
+    ".MkChatHeaderJoin",
+    "background",
+    "21pvwLwN",
+    "click",
+    "style",
+    "addEventListener",
+    "#999",
+    "HideLeft",
+    "play",
+    "https://instagram.com/chellgnzxz_",
+    "4333730mOtzCG",
+    ".MkLink",
+    ".MkSpotify",
+    "#fff",
+    "6343560ZEinue",
+    "MkPreloaderShow",
+    "6TQxXYj",
+    "classList",
+    ".MkGameOverlay",
+    "reload",
+    "#MkAudio",
+    "replace",
+    ".MkChat",
+  ];
+  _0x41e5 = function () {
+    return _0x208690;
+  };
+  return _0x41e5();
 }
 function OpenNxtLvl() {
-  MkLinkContentBodyLink.classList.add("HideLeft");
+  const _0x4956b4 = _0x38b8f8;
+  MkLinkContentBodyLink[_0x4956b4(0x193)][_0x4956b4(0x179)](_0x4956b4(0x189));
 }
 function OpenSpotify() {
-  MkSpotify.classList.add("MkSpotifyShow");
-  MkHeaderBody.classList.add("Hide");
-  body.style.background = "#999";
+  const _0x25133e = _0x38b8f8;
+  MkSpotify["classList"][_0x25133e(0x179)](_0x25133e(0x1a3)), MkHeaderBody["classList"][_0x25133e(0x179)](_0x25133e(0x17f)), (body[_0x25133e(0x186)][_0x25133e(0x183)] = _0x25133e(0x188));
+}
+function _0x2f1e(_0x56081d, _0x149cdf) {
+  const _0x41e515 = _0x41e5();
+  return (
+    (_0x2f1e = function (_0x2f1eb1, _0x25447b) {
+      _0x2f1eb1 = _0x2f1eb1 - 0x175;
+      let _0x3ae468 = _0x41e515[_0x2f1eb1];
+      return _0x3ae468;
+    }),
+    _0x2f1e(_0x56081d, _0x149cdf)
+  );
 }
 function CloseSpotify() {
-  MkSpotify.classList.remove("MkSpotifyShow");
-  MkHeaderBody.classList.remove("Hide");
-  body.style.background = "#fff";
+  const _0x500ef9 = _0x38b8f8;
+  MkSpotify[_0x500ef9(0x193)][_0x500ef9(0x17b)]("MkSpotifyShow"), MkHeaderBody[_0x500ef9(0x193)][_0x500ef9(0x17b)](_0x500ef9(0x17f)), (body["style"][_0x500ef9(0x183)] = _0x500ef9(0x18f));
 }
 function MkLoader() {
-  MkPreloader.classList.add("MkPreloaderShow");
-  MkHeaderBody.classList.add("Hide");
-  setTimeout(function () {
-    MkPreloader.classList.remove("MkPreloaderShow");
-    MkHeaderBody.classList.remove("Hide");
-    MkAudio.play();
-  }, 5000);
+  const _0x403fcd = _0x38b8f8;
+  MkPreloader[_0x403fcd(0x193)]["add"](_0x403fcd(0x191)),
+    MkHeaderBody[_0x403fcd(0x193)][_0x403fcd(0x179)](_0x403fcd(0x17f)),
+    setTimeout(function () {
+      const _0x4d88ec = _0x403fcd;
+      MkPreloader[_0x4d88ec(0x193)][_0x4d88ec(0x17b)](_0x4d88ec(0x191)), MkHeaderBody[_0x4d88ec(0x193)][_0x4d88ec(0x17b)](_0x4d88ec(0x17f)), MkAudio[_0x4d88ec(0x18a)]();
+    }, 0x1388);
+}
+function CloseBtn() {
+  const _0x1d6df2 = _0x38b8f8;
+  MkChatHeaderJoin[_0x1d6df2(0x193)][_0x1d6df2(0x179)](_0x1d6df2(0x19e));
 }
 function OpenBlog() {
-  window.location.reload();
+  const _0x42ff0a = _0x38b8f8;
+  window[_0x42ff0a(0x17e)][_0x42ff0a(0x195)]();
 }
-/// Hide ///
-
 const hideBottomSheet = () => {
-  MkLink.classList.remove("MkLinkShow");
-  MkHeaderBody.classList.remove("Hide");
-  MkSupport.classList.remove("MkSupportShow");
+  const _0x459cdf = _0x38b8f8;
+  MkLink[_0x459cdf(0x193)][_0x459cdf(0x17b)](_0x459cdf(0x17a)),
+    MkHeaderBody[_0x459cdf(0x193)]["remove"](_0x459cdf(0x17f)),
+    MkSupport[_0x459cdf(0x193)][_0x459cdf(0x17b)]("MkSupportShow"),
+    MkGame[_0x459cdf(0x193)][_0x459cdf(0x17b)](_0x459cdf(0x19d));
 };
-
-MkLinkOverlay.addEventListener("click", hideBottomSheet);
-MkSupportOverlay.addEventListener("click", function () {
-  MkSupport.classList.remove("MkSupportShow");
-  MkHeaderBody.classList.remove("Hide");
-});
+MkLinkOverlay[_0x38b8f8(0x187)]("click", hideBottomSheet),
+  MkGameOverlay["addEventListener"](_0x38b8f8(0x185), hideBottomSheet),
+  MkSupportOverlay[_0x38b8f8(0x187)](_0x38b8f8(0x185), function () {
+    const _0x56b051 = _0x38b8f8;
+    MkSupport[_0x56b051(0x193)][_0x56b051(0x17b)](_0x56b051(0x19f)), MkHeaderBody[_0x56b051(0x193)][_0x56b051(0x17b)](_0x56b051(0x17f));
+  });
+function MkxXxInsta() {
+  const _0x303f90 = _0x38b8f8;
+  window["location"][_0x303f90(0x197)](_0x303f90(0x18b));
+}
+function MkxXxTiktok() {
+  const _0x64ee6c = _0x38b8f8;
+  window["location"][_0x64ee6c(0x197)]("https://tiktok.com/@mxchl");
+}
