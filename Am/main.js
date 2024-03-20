891a1558f1c72dc890b866bc379fc37b89522292
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2024-03-21 02:34:32 +0700

    Update main.js

--- Am/main.js
+++ Am/main.js
@@ -360,19 +360,24 @@ function MkSpotifyPlaylistX() {
 
 document.onkeydown = function (e) {
   if (e.keyCode == 123) {
+    return false;
     window.location.replace("https://instagram.com/marchell_kvandra");
   }
   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
     window.location.replace("https://instagram.com/marchell_kvandra");
   }
   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
     window.location.replace("https://instagram.com/marchell_kvandra");
   }
   if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
     window.location.replace("https://instagram.com/marchell_kvandra");
   }
 
   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
     window.location.replace("https://instagram.com/marchell_kvandra");
   }
 };
